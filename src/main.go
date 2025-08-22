b4b2886b85430629f9de5456dea9e378d519da4e
desiderium-wav <g09784530@gmail.com>
2025-08-22 16:51:12 -0400

    Update main.go

--- src/main.go
+++ src/main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/joho/godotenv"
 )
+
+//starts http server
+func main() {
+	go func() {
+		http.HandleFunc("/", getRoot)
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+}
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("got / request\n")
+	io.WriteString(w, "Excalibur is at render.com now.. 🚀\n")
+}
 
 func main() {
 	godotenv.Load()
